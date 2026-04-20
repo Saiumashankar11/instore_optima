70fc107d06efd521b74b72bffd948652c4bb48be
Sai Uma Shankar Kesireddi <2487392@cognizant.com>
2026-04-20 16:10:44 +0530

    chore: Register ISupplierRepository and IPurchaseOrderRepository for Lekha

--- src/instore_optima.Api/Program.cs
+++ src/instore_optima.Api/Program.cs
@@ -9,6 +9,9 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
+builder.Services.AddScoped<IPurchaseOrderRepository, PurchaseOrderRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("instore_optima.Api")));
