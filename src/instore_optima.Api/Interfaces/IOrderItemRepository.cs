d46be6865a55973e639a2a247db7827dd63c353f
Reddy <2487193@cognizant.com>
2026-04-21 08:49:19 +0530

    feat: Add DTOs and Repositories for Orders, OrderItems, Payment

---
+++ src/instore_optima.Api/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using instore_optima.Domain.Entities;
+
+namespace instore_optima.Api.Repositories.Interfaces
+{
+    public interface IOrderItemRepository
+    {
+        Task<IEnumerable<Order_Items>> GetItemsByOrderIdAsync(int orderId);
+        Task<Order_Items?> GetOrderItemByIdAsync(int orderItemId);
+        Task<Order_Items> CreateOrderItemAsync(Order_Items item);
+        Task<Order_Items> UpdateOrderItemAsync(Order_Items item);
+        Task DeleteOrderItemAsync(int orderItemId);
+    }
+}
